0c7cfe33a34e458c96814afff4344ae3ac600700
YalingZheng <yaling.zheng@gmail.com>
2020-11-10 13:02:28 -0500

    Create testConsumerProducer.java

---
+++ Concurrency/JavaExer/testConsumerProducer.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class CoinProducer extends Thread {
+	Random r = new Random();
+	
+	private Coin c; 
+	
+	private int ID; 
+	
+	static int counter = 0;
+	
+	public CoinProducer(Coin c) {
+		this.c = c;
+		this.ID = ++counter;
+	}
+	
+	public void makeCoin(int amount) {
+		System.out.println("Current Coin: " + Coin.amount + " CoinProducer" + ID + " makes one coin ... ");
+		c.increase(amount);
+	}
+	
+	public void run() {		
+		try {
+			Thread.sleep(r.nextInt(10));
+			// wait for a random time
+			// consume one coin
+			makeCoin(1);			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
