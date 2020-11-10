41c2e94b6b63a5c5248bcea5a769e866cee03505
YalingZheng <yaling.zheng@gmail.com>
2020-11-10 09:37:58 -0500

    Using protected objects

---
+++ Concurrency/AdaExamples/main.adb
@@ -0,0 +1,46 @@
+with Ada.Containers.Synchronized_Queue_Interfaces;
+with Ada.Containers.Unbounded_Synchronized_Queues;
+with Ada.Text_IO;
+
+procedure Producer_Consumer_V1 is
+   type Work_Item is range 1 .. 30;
+
+   package Work_Item_Queue_Interfaces is
+     new Ada.Containers.Synchronized_Queue_Interfaces
+           (Element_Type => Work_Item);
+
+   package Work_Item_Queues is
+     new Ada.Containers.Unbounded_Synchronized_Queues
+           (Queue_Interfaces => Work_Item_Queue_Interfaces);
+
+   Queue : Work_Item_Queues.Queue;
+
+   task type Producer;
+   task type Consumer;
+
+   Producers : array (1 .. 1)  of Producer;
+   Consumers : array (1 .. 30) of Consumer;
+
+   task body Producer is
+   begin
+      Ada.Text_IO.Put_Line("Executing Task Producer");
+      for Item in Work_Item loop
+         Queue.Enqueue (New_Item => Item);
+         Ada.Text_IO.Put("Enqueue ");
+         Ada.Text_IO.Put_Line (Work_Item'Image (Item));
+      end loop;
+   end Producer;
+
+   task body Consumer is
+      Item : Work_Item;
+   begin
+      loop
+         Ada.Text_IO.Put("Dequeue ");
+         Queue.Dequeue (Element => Item);
+         Ada.Text_IO.Put_Line (Work_Item'Image (Item));
+      end loop;
+   end Consumer;
+
+begin
+   null;
+end Producer_Consumer_V1;
